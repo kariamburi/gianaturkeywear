6173a3920477336cd1d3e650848f99b7dd075a1d
Paul Irungu <paul.irungu@gmail.com>
2026-05-06 14:15:03 +0300

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,25 @@
 const nextConfig = {
-    images: {
-        domains: ['utfs.io','img.clerk.com'],
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'utfs.io',
-            port: ''
-          }
-        ]
+  images: {
+    domains: [
+      "utfs.io",
+      "img.clerk.com",
+      "firebasestorage.googleapis.com",
+    ],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "utfs.io",
       },
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
+      },
+      {
+        protocol: "https",
+        hostname: "firebasestorage.googleapis.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
